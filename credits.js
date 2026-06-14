658f2e4e8e01254a40a4b0eb6abcc42a65803199
Average_Medal_Enjoyer <128150229+AverageMedalEnjoyer@users.noreply.github.com>
2026-06-14 17:27:07 -0500

    add buncha credits

--- credits.js
+++ credits.js
@@ -1,10 +1,19 @@
 var FcDevTeam = [
     // format is: credit(name, image url, role, socials, custom font if any)
-    credit("guy with longg name", "assets/test.jpg", "Web Developer", "", "Quake"),
-    credit("guy with longg name", "assets/test.jpg", "tester", "", "Frak"),
-    credit("guy with longg name", "assets/test.jpg", "tester", "", "AndyBold"),
-    credit("guy with longg name", "assets/test.jpg", "tester", "", "ComicSans"),
-    credit("guy with longg name", "assets/test.jpg", "tester", "")
+    credit("Vvis", "assets/credits/vvis.png", "Lead", "", "Quake"),
+    credit("SaintSoftware", "assets/credits/saintsoftware.jpg", "Co-Lead", "", "Frak"),
+    credit("Average_Medal_Enjoyer", "assets/credits/medal.jpg", "Co-Lead, Lead Mapper", "", "ComicSans"),
+    credit("ArgGuard", "assets/credits/argguard.jpg", "Programmer", "", "AndyBold"),
+    credit("Gordon Freakman", "assets/credits/freakman.jpg", "Programmer, Modeller", "", "Quake"),
+    credit("YOW", "assets/credits/yow.gif", "Programmer, Mapper", "", "Frak"),
+    credit("Tyro (Xerin)", "assets/credits/xerin.jpg", "Mapper", "", "Quake"),
+    credit("ShiroDkxtro2", "assets/credits/shiro.jpg", "LUX, Shaders", "", "AndyBold"),
+    credit("Zeldaboy14", "assets/credits/zelda.jpg", "Programmer", "", "Quake"),
+    credit("Crowbar", "assets/credits/crowbar.jpg", "Artist", "", "Frak"),
+    credit("DudeTheNinja", "assets/credits/dudetheninja.jpg", "Modeller", "", "AndyBold"),
+    credit("Spyguy26", "assets/credits/spyguy.jpg", "Programmer, Modeller", "", "Quake"),
+    credit("Hotpockette", "assets/credits/hotpockette.jpg", "Animator", "", "Frak"),
+    credit("Fyzure", "assets/credits/fyzure.jpg", "Animator", "", "Quake")
 ];
 
 var Contributors = [
