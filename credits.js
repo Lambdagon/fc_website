43be88de10d877768c353eb3a09ed16017b6be42
Vvis <76018445+Lambdagon@users.noreply.github.com>
2026-06-18 19:39:22 -0600

    Update credits page
    - Updated fonts
    - Moved shiro to contrib as they are not a dev

--- credits.js
+++ credits.js
@@ -1,23 +1,22 @@
 var FcDevTeam = [
     // format is: credit(name, image url, role, socials, custom font if any)
-    credit("Vvis", "assets/credits/vvis.png", "Lead", "", "Quake"),
-    credit("SaintSoftware", "assets/credits/saintsoftware.jpg", "Co-Lead", "", "Frak"),
-    credit("Average_Medal_Enjoyer", "assets/credits/medal.jpg", "Co-Lead, Lead Mapper", "", "ComicSans"),
-    credit("ArgGuard", "assets/credits/argguard.jpg", "Programmer", "", "AndyBold"),
-    credit("Gordon Freakman", "assets/credits/freakman.jpg", "Programmer, Modeller", "", "Quake"),
-    credit("YOW", "assets/credits/yow.gif", "Programmer, Mapper", "", "Frak"),
-    credit("Tyro (Xerin)", "assets/credits/xerin.jpg", "Mapper", "", "Quake"),
-    credit("ShiroDkxtro2", "assets/credits/shiro.jpg", "LUX, Shaders", "", "AndyBold"),
-    credit("Zeldaboy14", "assets/credits/zelda.jpg", "Programmer", "", "Quake"),
-    credit("Crowbar", "assets/credits/crowbar.jpg", "Artist", "", "Frak"),
-    credit("DudeTheNinja", "assets/credits/dudetheninja.jpg", "Modeller", "", "AndyBold"),
-    credit("Spyguy26", "assets/credits/spyguy.jpg", "Programmer, Modeller", "", "Quake"),
-    credit("Hotpockette", "assets/credits/hotpockette.jpg", "Animator", "", "Frak"),
-    credit("Fyzure", "assets/credits/fyzure.jpg", "Animator", "", "Quake")
+    credit("Vvis", "assets/credits/vvis.png", "Project Lead", "", "futurot"),
+    credit("SaintSoftware", "assets/credits/saintsoftware.jpg", "Project Co-Lead", "", "Frak"),
+    credit("Average_Medal_Enjoyer", "assets/credits/medal.jpg", "Project Co-Lead, Lead Mapper", "", "ComicSans"),
+    credit("ArgGuard", "assets/credits/argguard.jpg", "Programmer", "", ""),
+    credit("Gordon Freakman", "assets/credits/freakman.jpg", "Programmer, Modeller", "", ""),
+    credit("YOW", "assets/credits/yow.gif", "Programmer, Mapper", "", "yow"),
+    credit("Tyro (Xerin)", "assets/credits/xerin.jpg", "Mapper", "", ""),
+    credit("Zeldaboy14", "assets/credits/zelda.jpg", "Programmer", "", ""),
+    credit("Crowbar", "assets/credits/crowbar.jpg", "Artist", "", "AndyBold"),
+    credit("DudeTheNinja", "assets/credits/dudetheninja.jpg", "Modeller", "", ""),
+    credit("Spyguy26", "assets/credits/spyguy.jpg", "Programmer, Modeller", "", ""),
+    credit("Hotpockette", "assets/credits/hotpockette.jpg", "Animator", "", ""),
+    credit("Fyzure", "assets/credits/fyzure.jpg", "Animator", "", "")
 ];
 
 var Contributors = [
-
+	credit("ShiroDkxtro2", "assets/credits/shiro.jpg", "LUX, Shaders", "", ""),
 ];
 
 // append dev team
